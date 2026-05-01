fcdd14430ca66cf58b6b10bbe1c7c9aae54d9ac3
reyhubcom <94319232+reyhubcom@users.noreply.github.com>
2026-05-01 07:50:19 -0400

    1

--- src/navigation copy.ts
+++ src/navigation copy.ts
@@ -3,7 +3,7 @@ import { getPermalink, getBlogPermalink, getAsset } from './utils/permalinks';
 export const headerData = {
   links: [
     {
-      text: 'Staking',
+      text: 'Homes',
       links: [
         {
           text: 'SaaS',
